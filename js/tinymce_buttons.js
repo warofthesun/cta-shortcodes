ab5b9af7f0cc82a16189730cdfcb8d71c246bcaa
Dan Brown <fireflip@gmail.com>
2018-08-29 15:51:36 -0500

    Turn CTA button into menu

--- js/tinymce_buttons.js
+++ js/tinymce_buttons.js
@@ -2,10 +2,22 @@
     tinymce.PluginManager.add('cta_buttons', function( ed, url ) {
       ed.addButton('cta-one', {
           text : 'Add CTA',
+          type: 'menubutton',
           icon : false,
-          onclick : function() {
-               ed.selection.setContent('[cta-one]' + ed.selection.getContent() + '[/cta-one]');
-          }
+          menu: [
+              {
+                text: 'Version 1',
+                onclick : function() {
+                     ed.selection.setContent('[cta-one]' + ed.selection.getContent() + '[/cta-one]');
+                }
+              },
+              {
+                text: 'Version 2',
+                onclick : function() {
+                     ed.selection.setContent('[cta-two]' + ed.selection.getContent() + '[/cta-two]');
+                }
+              }
+          ]
       });
 
     });
